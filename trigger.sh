847fbe5b62a060da49fded13592a42ece5963abe
aticio <aticiozgur@gmail.com>
2022-07-06 00:08:41 +0300

    temporary change

--- trigger.sh
+++ trigger.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp -rp /opt/tickrecorder/tickrecorder.db /tmp/
 sqlite3 -csv /tmp/tickrecorder.db "select * from tick_price;" > /tmp/tickrecorder.csv
-cd /opt/pi/
-pip install -r requirements.txt -U
-nohup python3 /opt/pi-optimizer/pi-optimizer.py > /dev/null 2> /dev/null < /dev/null &
+zip /tmp/tickrecorder.zip /tmp/tickrecorder.csv
+#cd /opt/pi/
+#pip install -r requirements.txt -U
+#nohup python3 /opt/pi-optimizer/pi-optimizer.py > /dev/null 2> /dev/null < /dev/null &
